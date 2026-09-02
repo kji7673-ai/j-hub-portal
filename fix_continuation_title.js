e6759f9ecf6ee5e99c8256b21bc33ca877dc3302
Kim Joongil <kji7673@gmail.com>
2026-09-02 20:29:14 +0900

    Hide continuation titles for seamless reading

---
+++ fix_continuation_title.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+let code = fs.readFileSync('book_data.js', 'utf8');
+let dataCode = code.replace(/const bookData =|var bookData =/g, 'global.bookData =');
+eval(dataCode);
+
+bookData.pages.forEach(p => {
+    if (p.title && p.title.includes("(계속)")) {
+        p.title = ""; // Make it empty so it seamlessly flows like a book page
+    }
+});
+
+const outCode = `var bookData = ${JSON.stringify(bookData, null, 4)};\n\nif (typeof module !== 'undefined' && module.exports) {\n    module.exports = bookData;\n}\n`;
+fs.writeFileSync('book_data.js', outCode, 'utf8');
