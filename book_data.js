899f971453fec069e7b0991b2f8bc0465df22604
Kim Joongil <kji7673@gmail.com>
2026-09-02 16:21:37 +0900

    Refine reflection text in '잘게 잘게 나누어 버리자'

--- book_data.js
+++ book_data.js
@@ -277,7 +277,7 @@ var bookData = {
             "part": "4부: 증언과 성찰",
             "partTitle": "[Theme 4] 미완의 일상",
             "title": "잘게 잘게 나누어 버리자",
-            "text": "슬프다 아프다의 감정에 <br>칼을 댄다 <br><br>아픈건 몸이 아플때 슬픈건 맘이 아플때 <br>근데 이건 슬프지만, 슬픈게 아니다 <br>그냥 서글픈 것이지 <br>그래 잠시 관계속에서 짓눌린거다 <br><br>이런 것을 슬프다라고 생각하지말자 <br><br>칼을 대어 도려내자 잘게 잘게 해체하여 형체를 없애자 <br>사실이란 덩어리만 남아있도록 <br><br>그렇게 감정에 칼을 대자<br><br>사실이란 덩어리만 덩그러니 남도록<br><br><div style=\"margin-top: 40px; padding: 24px; background-color: #f5f5f7; border-radius: 12px; color: #333333; font-size: 15px; line-height: 1.6; border-left: 3px solid #0066cc;\">요즘 만난 사람중에 매우 낯선 표현을 한 친구가 있다. \"기저귀차고 마이크에 노래 부르는 사람이 있다\" 이게 무슨 말일까 곰곰히 생각해보니, 미성숙한 사람이 자기 기분에 마이크 잡아 말을 하는것을 이렇게 비유했구나 싶다. 참으로 신랄한 표현이다. 나도 혹시 기저귀 차고 다 돌아다니는 것은 아닌지 조심해야겠다.</div>",
+            "text": "슬프다 아프다의 감정에 <br>칼을 댄다 <br><br>아픈건 몸이 아플때 슬픈건 맘이 아플때 <br>근데 이건 슬프지만, 슬픈게 아니다 <br>그냥 서글픈 것이지 <br>그래 잠시 관계속에서 짓눌린거다 <br><br>이런 것을 슬프다라고 생각하지말자 <br><br>칼을 대어 도려내자 잘게 잘게 해체하여 형체를 없애자 <br>사실이란 덩어리만 남아있도록 <br><br>그렇게 감정에 칼을 대자<br><br>사실이란 덩어리만 덩그러니 남도록<br><br><div style=\"margin-top: 40px; padding: 24px; background-color: #f5f5f7; border-radius: 12px; color: #333333; font-size: 15px; line-height: 1.6; border-left: 3px solid #0066cc;\">요즘 만난 사람중에 매우 낯선 표현을 한 친구가 있다. \"기저귀차고 마이크에 대고 노래 부르는 사람이 있다\" 이게 무슨 말일까 곰곰히 생각해보니, 미성숙한 사람이 자기 기분에 마이크에 대고 동네방네 자기 말 하는것을 이렇게 비유했구나 싶다. 참으로 신랄한 표현이다. 나도 혹시 기저귀 차고 막 돌아다니는 것은 아닌지 조심해야겠다.</div>",
             "partCategory": "2부: 증언과 성찰",
             "image": "static/images/user_20.jpg",
             "type": "image_top"
