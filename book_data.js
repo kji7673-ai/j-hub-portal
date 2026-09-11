2ce8a59febeee87f8943e7d3318aade26bb91018
Kim Joongil <kji7673@gmail.com>
2026-09-12 07:44:46 +0900

    Content: Replace images on pages 33 and 35 with gallery cuts

--- book_data.js
+++ book_data.js
@@ -295,7 +295,7 @@ var bookData = {
             "type": "image_top",
             "title": "오뚝이",
             "text": "<div style=\"text-align: center; max-width: 500px; margin: 0 auto;\">\n<p style=\"text-align: left; line-height: 1.85; font-size: 1.1em; color: #333; margin-top: 40px; margin-bottom: 60px; display: inline-block; word-break: keep-all;\">\n흔들릴지언정 넘어지지 않는,<br><br>\n그래서<br>\n재미 삼아 툭툭 건드려도 상관없는<br><br>\n난<br>\n중심 잡힌 오뚝이인 줄 알았습니다.<br><br>\n근데 이젠, 작은 입김에도 휘청이며 중심 잡기 위해<br>\n몹시도 흔들거리는, 아파서 흔들거리는 제가 되었습니다.<br><br>\n세월의 흐름 속에<br>\n내 안에 가라앉은 침잠된 무게가 중심추 되어,<br><br>\n이젠<br>\n웬만한 바람이 불어도<br>\n비록 흔들거릴지언정 넘어지지는 않는<br>\n난 오뚝이인데,<br>\n그래 그렇지.<br><br>\n내 속에 침잠된 그 묵직함이<br>\n오늘의 저를 지켜줍니다.\n</p>\n</div>\n\n<blockquote style=\"background: rgba(0, 0, 0, 0.04); border-radius: 8px; padding: 20px 24px; font-size: 1.0em; color: #555; line-height: 1.6; border: none; margin-top: 40px; text-align: left; font-weight: 500;\">\n<strong>오늘도 사랑한다로 시작합니다</strong><br><br>\n오늘 하루, 난 또 다른 모습으로 뭍의 삶을 살아갑니다. 태초의 쉼터인 물로 돌아가기 전까지 모두가 그렇게 살아갑니다. 특별할 것 없습니다. 매 맞는 말의 모습에서 메시아를 보았던 니체처럼, 언젠가 나도 이 고달픈 쟁이 삶의 끝에서 희열의 본질을 마주하겠지. 오늘도 저는 '사랑한다'는 말로 하루를 시작합니다. 그리고 하루의 마지막엔, '나는 사랑했다'로 끝을 맺고 싶습니다.\n</blockquote>",
-            "image": "static/images/sketches/26.jpg",
+            "image": "static/images/gallery_batch3_sphere_1789095532897.jpg",
             "part": "4부: 증언과 성찰",
             "partTitle": "[Theme 3] 설계의 본질에 대한 고민",
             "partCategory": "제2막: 나라는 개체의 독립과 생존",
@@ -317,7 +317,7 @@ var bookData = {
             "title": "요즘 잠을 잘 수가 없다",
             "text": "<div style=\"text-align: center; max-width: 500px; margin: 0 auto;\">\n<p style=\"text-align: left; line-height: 1.85; font-size: 1.1em; color: #333; margin-top: 40px; margin-bottom: 60px; display: inline-block;\">\n요즘 난 잠을 잘 수가 없습니다.<br><br>눈을 감으면,<br>저의 모든 감각 기관이 더욱 예민해집니다.<br>어느 날은 자고 있는 상태에서도<br>내 옆에 기어가는 벌레를 인지하고 잡은 적도 있습니다.<br><br>심지어<br><br>난 눈을 뜨고 잡니다.<br>자고 있는 나에게 말을 붙이면 대답을 합니다.<br><br>군에서는 선임들이 내 머리카락만 건드려도<br>관등성명을 댄다고 모두들 신기해했지만,<br>사실 난 자면서도 그들의 모든 일거수일투족을<br>인지하고 있었습니다.<br><br>저의 이 능력은 사실 지네에게 물렸을 때 생긴 능력입니다.<br><br>거미에 물렸다면 스파이더맨이 될 수도 있었는데 아깝습니다.<br>아무튼, 난 지네맨이 되었습니다.<br><br>지네가 가진 예민한 촉각을 제가 가지게 된 것입니다.<br>몹시 예민합니다.<br>이런 능력을 가진 후 몇 가지 단점도 생겼습니다.<br><br>첫째는, 밤에 예민하다 보니 낮에 몹시 둔하다는 것입니다.<br>말도 행동도 느리고, 쉽게 다른 사람의 말을 알아듣지도 못하게 되었습니다.<br><br>둘째, 너무 많은 정보가 내게 쏟아져 들어옵니다. 주변의 모든 정보가.<br>그래서, 난 더 둔해집니다.<br>화를 낼 일도 한참 나중에 되어서야<br>'아! 그때 화를 냈어야 했는데' 하고 후회하곤 합니다.<br><br>결국 난 몹시 예민하지만, 몹시도 둔한 사람이 되었습니다.\n</p>\n</div>\n\n<blockquote style=\"background: rgba(0, 0, 0, 0.04); border-radius: 8px; padding: 20px 24px; font-size: 0.9em; color: #555; line-height: 1.6; border: none; margin-top: 40px;\">\n아직도 전 어떨 땐 너무 둔하고, 또 어떨 땐 너무 예민하게 신경이 곤두서서 나도 모르게 손발이 떨릴 때가 있습니다. 긴장해서 말마저 더듬거릴 때면, 저는 속으로 중얼거립니다.<br><br>\n'그래, 나는 지네맨이었지. 아니면 지구에 불시착한 외계인이거나.'<br><br>\n그렇게 나 스스로를 세뇌하며 버텨냅니다. 참 하루하루가 고단하고 쉽지 않습니다. 어쩌면 나만 이 이상한 능력이 있지는 않을 것 같습니다.<br><br>\n다른 사람들도 겉으로는 무표정해 보이지만, 속으로는 상처받지 않기 위해 온갖 더듬이를 세운 채 하루를 간신히 버텨내는 또 다른 지네맨들이 아닐까 싶습니다.<br><br>\n둔감함이라는 갑옷을 입고 살아가는 세상의 모든 이상 능력 소유자들에게 연대의 위로를 건네봅니다.\n</blockquote>",
             "partCategory": "제2막: 나라는 개체의 독립과 생존",
-            "image": "static/images/sketches/101.jpg",
+            "image": "static/images/gallery_batch4_flat_1789095642326.jpg",
             "type": "poem",
             "caption": "오리지널 스케치 도면 (Original Sketch Drawing)"
         },
